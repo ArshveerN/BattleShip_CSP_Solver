b83edf04da1c3604b45bcb45d6c5e3c79102d295
ArshveerN <arshveernagra1@gmail.com>
2025-11-08 20:56:26 -0500

    git script

---
+++ gitscript.ps1
@@ -0,0 +1,10 @@
+git add ./backtracking.py
+echo committed backtracking
+git add ./battle.py
+echo committed battle
+git add ./constraints.py csp.py
+echo committed constrainsts
+git commit -m "files"
+echo commited
+git push
+echo done
